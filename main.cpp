05021c232da61e331d7f0e834680295d395f8d2b
develjacob <mcjacob61@gmail.com>
2018-03-02 18:47:23 -0900

    meaningful commit message, add function named addAB

--- main.cpp
+++ main.cpp
@@ -6,8 +6,16 @@
 using std::cout;
 using std::endl;
 
+int addAB(int a, int b){
+	return a+b;
+}
+
 int main(){
 
 	cout << "Hello World" << endl;
+
+	int value = addAB(3,5);
+
+	cout << value << endl;
 	return 0;
 }
